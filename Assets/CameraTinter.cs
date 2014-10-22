df9b487f5a32371b2e38ac426f3eadae5d76ac65
Zac <brickman1444@gmail.com>
2014-10-21 20:04:32 -0500

    Color tinting and water movement with O and P.

---
+++ Assets/CameraTinter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTinter : MonoBehaviour {
+
+    public GameObject waters;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (waters.transform.position.y >= Camera.main.transform.position.y)
+        {
+            guiTexture.enabled = true;
+        }
+        else
+        {
+            guiTexture.enabled = false;
+        }
+	}
+}
---
+++ Assets/Scripts/WaterMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMover : MonoBehaviour {
+
+    public float minY;
+    public float maxY;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = transform.position;
+        pos.y = minY;
+        transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+	}
+}
