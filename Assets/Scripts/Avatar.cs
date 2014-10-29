60a2170345820b8a4d4a288e4f52aa402bf040a7
Zac <brickman1444@gmail.com>
2014-10-28 20:00:19 -0500

    Click on leaks to stop them.

---
+++ Assets/Scripts/Avatar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Avatar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray();
+            ray.origin = Camera.main.transform.position;
+            ray.direction = Camera.main.transform.forward;
+
+            RaycastHit outHitInfo;
+
+            if (Physics.Raycast(ray, out outHitInfo, 2.5f, 1 << 8))
+            {
+                Destroy(outHitInfo.collider.gameObject);
+                Debug.Log("Hit!");
+            }
+            else
+            {
+                Debug.Log("Miss!");
+            }
+        }
+	}
+}
