f8e7d23b34be45c86b6e84376da80ddef0722ccc
Zac <brickman1444@gmail.com>
2014-11-25 16:12:19 -0600

    Background for main menu

---
+++ Assets/GUITextureFillScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUITextureFillScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float width = Screen.width;
+        float height = Screen.height;
+
+        guiTexture.pixelInset.Set(-width, -height, width, height);
+	}
+}
