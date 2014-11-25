ebbb7b7050a251e38e9bdb65bd3351a8f9158bb3
Zac <brickman1444@gmail.com>
2014-11-25 01:43:19 -0600

    Screen lock the mouse.

---
+++ Assets/Scripts/ScreenLock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenLock : MonoBehaviour {
+
+    public bool lockCursor = true;
+
+	// Use this for initialization
+	void Start () {
+        Screen.lockCursor = lockCursor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
